13ebfdf4fd36605f4d07a17da30a5accca998acf
YPV0 <yuriisfw@gmail.com>
2023-05-02 15:08:21 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,71 +6,76 @@ import { fetchCountries } from './js/fetchCountries';
 const DEBOUNCE_DELAY = 300;
 const MAX_COUNTRIES = 10;
 
-const searchBox = document.getElementById('search-box');
-const countryList = document.querySelector('.country-list');
-const countryInfo = document.querySelector('.country-info');
-
-const renderCountryList = countries => {
-  const html = countries
-    .map(
-      country => `
-    <div class="country-item">
-      <img src="${country.flags.svg}" alt="Flag of ${country.name.common}" width="20"> 
-      <span>${country.name.common}</span>
-    </div>
-  `
-    )
-    .join('');
-  countryList.innerHTML = html;
+const refs = {
+  searchBox: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
 };
 
-const renderCountryInfo = country => {
+refs.searchBox.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput(event) {
+  const searchTerm = event.target.value.trim();
+  if (!searchTerm) {
+    clearAll();
+    return;
+  }
+  fetchCountries(searchTerm).then(showResults).catch(showError);
+}
+
+function showResults(countries) {
+  if (countries.length > MAX_COUNTRIES) {
+    Notify.info('Too many matches found. Please enter a more specific name.');
+    clearAll();
+  } else if (countries.length > 1) {
+    renderCountryList(countries);
+    clearInfo();
+  } else {
+    renderCountryInfo(countries[0]);
+    clearList();
+  }
+}
+
+function showError(error) {
+  Notify.failure('Oops, there is no country with that name');
+  clearAll();
+}
+
+function renderCountryList(countries) {
+  const html = countries.map(renderCountryItem).join('');
+  refs.countryList.innerHTML = html;
+}
+
+function renderCountryItem(country) {
+  const { flags, name } = country;
+  return `
+    <div class="country-item">
+      <img src="${flags.svg}" alt="Flag of ${name.common}" width="20"> 
+      <span>${name.common}</span>
+    </div>
+  `;
+}
+
+function renderCountryInfo(country) {
   const { flags, name, capital, population, languages } = country;
-  countryInfo.innerHTML = `
+  refs.countryInfo.innerHTML = `
     <img src="${flags.svg}" alt="Flag of ${name.common}" width="400">
     <h2>${name.common}</h2>
     <p>Capital: ${capital[0]}</p>
     <p>Population: ${population}</p>
     <p>Languages: ${Object.values(languages).join(', ')}</p>
   `;
-};
-
-const onInput = debounce(event => {
-  const searchTerm = event.target.value.trim();
-  if (!searchTerm) {
-    clearAll();
-    return;
-  }
-  fetchCountries(searchTerm)
-    .then(countries => {
-      if (countries.length > MAX_COUNTRIES) {
-        Notify.info(
-          'Too many matches found. Please enter a more specific name.'
-        );
-      } else if (countries.length > 1) {
-        renderCountryList(countries);
-        clearInfo();
-      } else {
-        renderCountryInfo(countries[0]);
-        clearList();
-      }
-    })
-    .catch(error => {
-      Notify.failure('Oops, there is no country with that name');
-      clearAll();
-    });
-}, DEBOUNCE_DELAY);
+}
 
 function clearInfo() {
-  countryInfo.innerHTML = '';
+  refs.countryInfo.innerHTML = '';
 }
+
 function clearList() {
-  countryList.innerHTML = '';
+  refs.countryList.innerHTML = '';
 }
 
 function clearAll() {
   clearInfo();
   clearList();
 }
-
-searchBox.addEventListener('input', onInput);
